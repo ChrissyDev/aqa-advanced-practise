a6b1bafab8532998012b954d77063291e78c0e23
ChrissyDev <stg.team01@gmail.com>
2024-03-11 18:27:19 +0200

    installing cypress

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: '', 
+    viewportWidth: 1280, 
+    viewportHeight: 720, 
+    defaultCommandTimeout: 4000, 
+    retries: { runMode: 1, openMode: 1}, 
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
